cc2687a26fc9932473477515ab6a644b569a6f72
lee00286 <crocklee1234@gmail.com>
2023-06-19 16:16:36 -0400

    feat: add dashboard page

--- test-admin/src/App.tsx
+++ test-admin/src/App.tsx
@@ -4,6 +4,7 @@ import { UserList } from './users';
 import { PostCreate, PostEdit, PostList } from './posts';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
+import { Dashboard } from './Dashboard';
 
 /**
  * Data Provider: Adapter that allows react-admin to talk to your API
@@ -12,7 +13,7 @@ import UserIcon from '@mui/icons-material/Group';
  *                     to display the list of users (not used in production)
  */
 export const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource
       name="posts"
       list={PostList}
